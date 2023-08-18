23889a5e62f06bed72e63f0486ef67aae42e8e79
Md. Jamal Hossen <joybd.eee@gmail.com>
2023-08-18 11:16:33 +0600

    login page done

--- src/app/(maniLayout)/login/page.jsx
+++ src/app/(maniLayout)/login/page.jsx
@@ -2,74 +2,146 @@
 import Link from 'next/link';
 import Lottie from "lottie-react";
 import LoginLotti from "../../../../public/Lottifiles/Signup.json";
-import React from 'react';
-import { BsGithub, BsGoogle } from 'react-icons/bs';
+import React, { useContext, useState } from 'react';
+import { BsFacebook, BsGoogle } from 'react-icons/bs';
+import { UserContext } from '@/context/AuthContext';
+// import { useLocation, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import {
+  FaLock,
+  FaRegEye,
+  FaRegEyeSlash,
+} from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div className="hero bg-base-200">
-        <div className="hero-content flex-col lg:flex-row gap-5">
-          <div className="w-full md:max-w-[60%] text-center lg:text-left">
-            <Lottie animationData={LoginLotti} loop={true} />
-          </div>
-          <div className="w-full md:max-w-[40%] card flex-shrink-0  max-w-sm  bg-base-100 py-10">
-            <form className="w-4/5 mx-auto relative">
-            <h3 className="text-center text-2xl font-semibold">Login To Fill Fussion</h3>
-            <div className='flex gap-5 justify-center py-8'>
-                <div className='py-2 px-5 bg-rose-500 rounded-lg'>
-                   <h3>Google</h3> 
-                </div>
-                <div className='py-2 px-5 bg-cyan-500 rounded-lg'>
-                    <h3>Facebook</h3>
-                </div>
+
+  const { userSignIn, googleSignIn, facebookLogin  } = useContext(UserContext)
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+  
+
+  const handleUserSignIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    userSignIn(email, password)
+      .then(result => {
+        setError("")
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'User Login has been success',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+      .catch(err => setError(err));
+  }
+
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then(result => {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'User Login has been success',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+      .catch(err => setError(err));
+  }
+
+  const handleFacebookLogin = () => {
+    facebookLogin()
+      .then(restul => { })
+      .catch(err => setError(err));
+  }
+
+  const hnadleShow = () => {
+    setShow(!show);
+  };
+
+  return (
+    <div className="hero bg-base-200">
+      <div className="hero-content flex-col lg:flex-row gap-5">
+        <div className="w-full md:max-w-[60%] text-center lg:text-left">
+          <Lottie animationData={LoginLotti} loop={true} />
+        </div>
+        <div className="w-full md:max-w-[40%] card flex-shrink-0  max-w-sm  bg-base-100 py-10">
+          <form onSubmit={handleUserSignIn} className="w-4/5 mx-auto relative">
+            <h3 className="text-center text-2xl font-semibold text-transparent bg-gradient-to-tr bg-clip-text from-blue-500 via-pink-500 to-red-500 dark:from-sky-300 dark:via-pink-300 dark:to-red-500">Login To File Fussion</h3>
+
+            <div className="flex gap-5 justify-center py-8">
+              <div className="py-2 px-5 bg-rose-500 rounded-lg">
+                <button onClick={handleGoogleSignIn} className="flex items-center gap-2 text-white"><BsGoogle className="mt-1" />Google</button>
+              </div>
+              <div className="py-2 px-5 bg-cyan-500 rounded-lg">
+                <button onClick={handleFacebookLogin} className="flex items-center gap-2 text-white"> <BsFacebook /> <h3>Facebook</h3> </button>
+              </div>
             </div>
             <div className="text-center flex justify-center items-center gap-3">
-                <div className="w-full h-0.5 bg-slate-600"></div>
-                <h2 className="font-semibold">or</h2>
-                <div className="w-full h-0.5  bg-slate-600"></div>
+              <div className="w-full h-0.5 bg-slate-600"></div>
+              <h2 className="font-semibold">or</h2>
+              <div className="w-full h-0.5  bg-slate-600"></div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="text-xl">Email</span>
+              </label>
+              <div className="">
+                <input
+                  className="w-full py-2  px-3  border rounded-full"
+                  type="email"
+                  name="email"
+                  placeholder="Enter Your Email"
+                  required
+                />
               </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="text-xl">Email</span>
-                </label>
-                <div className="">
-                  <input
-                    className="w-full py-2  px-3  border rounded-full"
-                    type="email"
-                    name="email"
-                    placeholder="Enter Your Email"
-                    required
-                  />
+            </div>
+            <div className="form-control my-2">
+              <label className="label">
+                <span className="text-xl">Password</span>
+              </label>
+              <div className='flex items-center relative'>
+                {/* <FaLock className="ml-2"></FaLock> */}
+                <input
+                  className="w-full py-2  px-3  border rounded-full"
+                  type={!show ? "password" : "text"}
+                  name="password"
+                  placeholder="Enter Your Password"
+                  required
+                />
+                <div onClick={hnadleShow} className="right-5 absolute">
+                  {!show ? (
+                    <FaRegEye className="text-xl"></FaRegEye>
+                  ) : (
+                    <FaRegEyeSlash className="text-xl"></FaRegEyeSlash>
+                  )}
                 </div>
               </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="text-xl">Password</span>
-                </label>
-                  <input
-                    className="w-full py-2  px-3  border rounded-full"
-                    type='password'
-                    name="password"
-                    placeholder="Enter Your Password"
-                    required
-                  />
-              </div>
-              <button className="bg-cyan-500 w-full text-white text-sm uppercase rounded-full  px-6 py-4 my-4">
-                Login
-              </button>
-              <div className="text-center  py-2">
-                <h2>
-                  Do not have an accoutn? Please{" "}
-                  <Link className="text-blue-600" href="/signup">
-                    Create an account
-                  </Link>
-                </h2>
-              </div>
-            </form>
-          </div>
+            </div>
+            {error ? <p className="text-red-600">incorrent password</p> : ""}
+            <button className="bg-cyan-500 w-full text-white text-sm uppercase rounded-full  px-6 py-4 my-4">
+              Login
+            </button>
+            <div className="text-center  py-2">
+              <h2>
+                Do not have an accoutn? Please{" "}
+                <Link className="text-blue-600" href="/signup">
+                  Create an account
+                </Link>
+              </h2>
+            </div>
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,40 @@
 "use client";
 
+import { UserContext } from "@/context/AuthContext";
 import Link from "next/link";
+import { useContext } from "react";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
+  const { user, logOut } = useContext(UserContext)
+
+
+
+  const handleLogOut = () => {
+    logOut()
+      .then(result => {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'User Logout has been success',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+  }
   return (
     <nav className="w-full h-20 bg-[#FFFFFF]">
       <div className="w-full container mx-auto flex flex-col md:flex-row justify-between items-center gap-4 p-5">
         <div>
           <Link href={'/'}>
-          <h1 class="text-2xl font-extrabold text-center lg:text-2xl 2xl:text-2xl">
-            <span class="text-transparent bg-gradient-to-br bg-clip-text from-teal-500 via-indigo-500 to-sky-500 dark:from-teal-200 dark:via-indigo-300 dark:to-sky-500">
-              File
-            </span>
-            <span class="text-transparent bg-gradient-to-tr bg-clip-text from-blue-500 via-pink-500 to-red-500 dark:from-sky-300 dark:via-pink-300 dark:to-red-500">
-              Fusion
-            </span>
-          </h1>
+            <h1 className="text-2xl font-extrabold text-center lg:text-2xl 2xl:text-2xl">
+              <span className="text-transparent bg-gradient-to-br bg-clip-text from-teal-500 via-indigo-500 to-sky-500 dark:from-teal-200 dark:via-indigo-300 dark:to-sky-500">
+                File
+              </span>
+              <span className="text-transparent bg-gradient-to-tr bg-clip-text from-blue-500 via-pink-500 to-red-500 dark:from-sky-300 dark:via-pink-300 dark:to-red-500">
+                Fusion
+              </span>
+            </h1>
           </Link>
         </div>
         <div>
@@ -34,18 +53,48 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <div className="flex gap-3">
-          <Link href={'/login'}>
-          <button className="py-2 px-5 border border-black rounded-xl ">
-            Login
-          </button>
-          </Link>
-         <Link href={'/signup'}>
-         <button className="py-2 px-5 border bg-cyan-500 rounded-xl ">
-            Sign Up
-          </button>
-         </Link>
-        </div>
+        {
+          user ?
+            <>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar mr-20">
+                  <div className="w-10 rounded-full">
+                    <img src={user?.photoURL} />
+                  </div>
+                </label>
+                <ul tabIndex={0} className="bg-cyan-500 mt-3 ml-3 text-white z-[1] p-2 shadow menu menu-sm dropdown-content rounded-box w-52">
+                  <li className="">
+                    <label tabIndex={0} className="avatar ">
+                      <div className="w-10 ml-12 rounded-full">
+                        <img className="" src={user?.photoURL} />
+                      </div>
+                    </label>
+                  </li>
+                  <li>
+                    <a className="justify-between">
+                      {user?.displayName}
+                    </a>
+                  </li>
+                  <li><a>Dashboard</a></li>
+                  <li onClick={handleLogOut}><a>Logout</a></li>
+                </ul>
+              </div>
+
+            </>
+
+            : <div className="flex gap-3">
+              <Link href={'/login'}>
+                <button className="py-2 px-5 border border-black rounded-xl ">
+                  Login
+                </button>
+              </Link>
+              <Link href={'/signup'}>
+                <button className="py-2 px-5 border bg-cyan-500 rounded-xl ">
+                  Sign Up
+                </button>
+              </Link>
+            </div>
+        }
       </div>
     </nav>
   );
